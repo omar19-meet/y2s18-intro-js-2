ef479bcec6a094e4b532cdb95c3f14ae78fe01a7
meet <omar19@meet.mit.edu>
2018-08-01 07:17:49 -0400

    lol

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,11 @@
 // Create your 'me' object literal here!
+var me= {
+firstName:"Omar",
+lastName:"Dahleh",
+favoriteFoods:["Steak Fillet", "Wara' Dawali", "Watermelon"],
+age:16,
+picture:"https://www.pbh2.com/wordpress/wp-content/uploads/2013/11/shiba-inu-puppy.jpg"
+
+}
+me = JSON.stringify(me)
+document.write(me)
